453d4574e0e8bfbd144c18ccebb786d7320aa668
dbrlwns <jkk4741@naver.com>
2025-04-08 15:11:27 +0900

    Initializing env

---
+++ ex1.py
@@ -0,0 +1,10 @@
+from pythonping import ping
+ 
+# sudo python ex1.py 로 해야하넹..
+
+ping("8.8.8.8", verbose=True, count=1)
+ping("100.100.100.255", verbose=True, count=1)
+
+
+
+# 사용할때마다 conda activate py3.9로 가상환경을 활성화시켜야함.
